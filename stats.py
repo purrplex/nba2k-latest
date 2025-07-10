b6084abcf966a821e71107a6db9f3be38a555d07
dd <dannniellyyy@gmail.com>
2025-07-10 01:00:47 -0700

    added the stats to stats

--- stats.py
+++ stats.py
@@ -1,5 +1,52 @@
 PLAYER_STATS = {}
 
 PLAYER_STATS['luka'] = {
-    'shoot_chance': 70,    
+    'shoot_chance': 50,
+    'pass_chance': 50,   
+    'ft_chance': 75,       
+}
+PLAYER_STATS['lebron'] = {
+    'shoot_chance': 60,
+    'pass_chance': 40,   
+    'ft_chance': 73,       
+}
+PLAYER_STATS['reeves'] = {
+    'shoot_chance': 70,
+    'pass_chance': 30,   
+    'ft_chance': 86,       
+}
+PLAYER_STATS['kobe'] = {
+    'shoot_chance': 90,
+    'pass_chance': 10,   
+    'ft_chance': 83,       
+}
+PLAYER_STATS['hachi'] = {
+    'shoot_chance': 30,
+    'pass_chance': 70,   
+    'ft_chance': 76,       
+}
+PLAYER_STATS['brunson'] = {
+    'shoot_chance': 35,
+    'pass_chance': 65,   
+    'ft_chance': 82,       
+}
+PLAYER_STATS['hart'] = {
+    'shoot_chance': 90,
+    'pass_chance': 10,   
+    'ft_chance': 75,       
+}
+PLAYER_STATS['melo'] = {
+    'shoot_chance': 10,
+    'pass_chance': 90,   
+    'ft_chance': 81,       
+}
+PLAYER_STATS['og'] = {
+    'shoot_chance': 75,
+    'pass_chance': 25,   
+    'ft_chance': 76,       
+}
+PLAYER_STATS['kat'] = {
+    'shoot_chance': 25,
+    'pass_chance': 75,   
+    'ft_chance': 83,       
 }
